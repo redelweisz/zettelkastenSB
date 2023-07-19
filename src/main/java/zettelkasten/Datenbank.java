9752f8bb6798348dafd887803861f100d6518663
redelweisz <canaro1312@gmail.com>
2023-07-19 11:42:41 +0200

    all tables implemented added method to search all zettel and save references in zettelBuzzwords when inserting buzzwords

--- src/main/java/zettelkasten/Datenbank.java
+++ src/main/java/zettelkasten/Datenbank.java
@@ -3,10 +3,12 @@ package zettelkasten;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.nio.ByteBuffer;
 import java.sql.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.UUID;
 
 
 public class Datenbank {
@@ -41,7 +43,11 @@ public class Datenbank {
 
     public static void insertBuzzword(Buzzword b) {
         try (Connection connection = DriverManager.getConnection(connectionString)) {
-            String query = "INSERT INTO buzzword (BuzzwordId, Name) VALUES (?, ?)";
+            //zettelBuzzwordId generieren
+            byte[] zettelBuzzwordId = generateZettelBuzzwordId();
+
+            //Query vorbereiten
+            String query = "INSERT INTO buzzwords (BuzzwordId, Name) VALUES (?, ?)";
             PreparedStatement statement = connection.prepareStatement(query);
 
 
@@ -53,14 +59,48 @@ public class Datenbank {
             // ausführen
             statement.executeUpdate();
 
-            // schließen
-            statement.close();
-        }catch (SQLException e){
+            // In headers und text nach buzzword suchen
+            String searchText = "%" + b.getName() + "%";
+            String searchQuery = "SELECT DISTINCT ZettelId FROM zettel WHERE Header LIKE ? OR Text LIKE ?";
+            PreparedStatement searchStatement = connection.prepareStatement(searchQuery);
+            searchStatement.setString(1, searchText);
+            searchStatement.setString(2, searchText);
+            ResultSet resultSet = searchStatement.executeQuery();
+
+            // Query um zu überprüfen, ob das Mapping bereits in zettelBuzzwords existiert
+            String checkMappingQuery = "SELECT COUNT(*) AS count FROM zettelBuzzwords WHERE ZettelId = ? AND BuzzwordId = ?";
+            PreparedStatement checkMappingStatement = connection.prepareStatement(checkMappingQuery);
+            checkMappingStatement.setBytes(2, b.getBuzzwordId());
+
+            // Einfügen, wenn nicht
+            String zettelBuzzwordInsertQuery = "INSERT INTO zettelBuzzwords (ZettelBuzzwordId, ZettelId, BuzzwordId) VALUES (?, ?, ?)";
+            PreparedStatement zettelBuzzwordStatement = connection.prepareStatement(zettelBuzzwordInsertQuery);
+            zettelBuzzwordStatement.setBytes(1, zettelBuzzwordId);
+            zettelBuzzwordStatement.setBytes(3, b.getBuzzwordId());
+
+            while (resultSet.next()) {
+                byte[] zettelId = resultSet.getBytes("ZettelId");
+                checkMappingStatement.setBytes(1, zettelId);
+                ResultSet checkResult = checkMappingStatement.executeQuery();
+                checkResult.next();
+                int count = checkResult.getInt("count");
+
+                if (count == 0) {
+                    zettelBuzzwordStatement.setBytes(2, zettelId);
+                    zettelBuzzwordStatement.executeUpdate();
+                }
+            }
+            System.out.println("made entry in zettelBuzzwords");
+
+            zettelBuzzwordStatement.close();
+            checkMappingStatement.close();
+            searchStatement.close();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public static ObservableList<Zettel> getZettelData() {
+    public static ObservableList<Zettel> getZettelData(){
         ObservableList<Zettel> zettelData = FXCollections.observableArrayList();
 
         try (Connection conn = DriverManager.getConnection(connectionString);
@@ -95,7 +135,7 @@ public class Datenbank {
 
     public void updateZettel(String header, String text, byte[] zettelId) {
         try (Connection conn = DriverManager.getConnection(connectionString);
-             PreparedStatement stmt = conn.prepareStatement("UPDATE Zettel SET Header = ?, Text = ? WHERE ZettelId = ?")) {
+             PreparedStatement stmt = conn.prepareStatement("UPDATE zettel SET Header = ?, Text = ? WHERE ZettelId = ?")) {
             stmt.setString(1, header);
             stmt.setString(2, text);
             stmt.setBytes(3, zettelId);
@@ -104,6 +144,14 @@ public class Datenbank {
         } catch (SQLException e) {
             System.out.println("Error updating zettel: " + e.getMessage());
         }
+    }
+
+    public static byte[] generateZettelBuzzwordId() {
+        UUID uuid = UUID.randomUUID();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(16);
+        byteBuffer.putLong(uuid.getMostSignificantBits());
+        byteBuffer.putLong(uuid.getLeastSignificantBits());
+        return byteBuffer.array();
     }
 
 
